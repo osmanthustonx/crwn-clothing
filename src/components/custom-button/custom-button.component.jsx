e35c934945ae3ac7f6b348fa16993ccbac47297d
Kuei <vx3012564897@gmail.com>
2020-02-29 19:14:02 +0800

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -6,7 +8,6 @@ import './custom-button.styles.scss';
 const CustomButton = ({
   children, type, isGoogleSign, ...otherProps
 }) => (
-  // eslint-disable-next-line react/button-has-type
   <button className={`${isGoogleSign ? 'google-sign-in' : ''} custom-button`} type={type} {...otherProps}>
     {children}
   </button>
@@ -15,6 +16,11 @@ const CustomButton = ({
 CustomButton.propTypes = {
   children: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  isGoogleSign: PropTypes.bool,
+};
+
+CustomButton.defaultProps = {
+  isGoogleSign: undefined,
 };
 
 export default CustomButton;
