fcc498b177f6c5e8223d8de960c37b012467584f
Kuei <vx3012564897@gmail.com>
2020-03-09 22:13:22 +0800

    added addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,23 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { connect } from 'react-redux';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.action';
+
 import './checkout-item.styles.scss';
 
 const CheckoutItem = ({
-  cartItem: {
+  cartItem, clearItem, addItemAction, removeItemAction,
+}) => {
+  const {
     name, imageUrl, price, quantity,
-  },
-}) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="itemImage" />
+  } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="itemImage" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div
+          role="button"
+          tabIndex="0 "
+          className="arrow"
+          onClick={() => removeItemAction(cartItem)}
+          onKeyPress={() => removeItemAction(cartItem)}
+        >
+        &#10094;
+
+        </div>
+        <div className="value">{quantity}</div>
+        <div
+          role="button"
+          tabIndex="0"
+          className="arrow"
+          onClick={() => addItemAction(cartItem)}
+          onKeyPress={() => addItemAction(cartItem)}
+        >
+&#10095;
+
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div
+        role="button"
+        tabIndex="0"
+        className="remove-button"
+        onClick={() => clearItem(cartItem)}
+        onKeyPress={() => clearItem(cartItem)}
+      >
+      &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+  );
+};
 
 CheckoutItem.propTypes = {
   cartItem: PropTypes.shape({
@@ -26,6 +62,15 @@ CheckoutItem.propTypes = {
     price: PropTypes.number,
     quantity: PropTypes.number,
   }).isRequired,
+  clearItem: PropTypes.func.isRequired,
+  addItemAction: PropTypes.func.isRequired,
+  removeItemAction: PropTypes.func.isRequired,
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItemAction: (item) => dispatch(addItem(item)),
+  removeItemAction: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
