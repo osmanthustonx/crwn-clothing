a954e1eb2c9af6b07eeaa7ad82c5d02c4c19c5b0
Kuei <vx3012564897@gmail.com>
2020-03-23 11:30:07 +0800

    context api useContext version

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
@@ -7,33 +7,24 @@ import CollectionsContext from '../../contexts/collections/collections.context';
 
 import './collection.styles.scss';
 
-const Collection = ({ match }) => (
-  <CollectionsContext.Consumer>
-    {
-      (collections) => {
-        const collection = collections[match.params.collectionId];
-        const { title, items } = collection;
-        return (
-          <div className="collection-page">
-            <h2 className="title">{title}</h2>
-            <div className="items">
-              {
-                items.map((item) => <CollectionItem key={item.id} item={item} />)
-              }
-            </div>
-          </div>
-        );
-      }
-    }
-  </CollectionsContext.Consumer>
-);
+const Collection = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {
+          items.map((item) => <CollectionItem key={item.id} item={item} />)
+        }
+      </div>
+    </div>
+  );
+};
 
 Collection.propTypes = {
-  collection: PropTypes.shape({
-    title: PropTypes.string,
-    items: PropTypes.array,
-  }).isRequired,
-  match: PropTypes.objectOf(PropTypes.object).isRequired,
+  match: PropTypes.objectOf(Object).isRequired,
 };
 
 export default Collection;
