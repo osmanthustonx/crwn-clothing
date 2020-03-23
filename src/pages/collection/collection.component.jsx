05b06a67b229950eab410106487d9c7e3d3e8b41
Kuei <vx3012564897@gmail.com>
2020-03-23 11:21:22 +0800

    context api Component.consumer version

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import { selectCollection } from '../../redux/shop/shop.selector';
+import CollectionsContext from '../../contexts/collections/collections.context';
 
 import './collection.styles.scss';
 
-const Collection = ({ collection }) => {
-  const { title, items } = collection;
-  return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
-        {
-          items.map((item) => <CollectionItem key={item.id} item={item} />)
-        }
-      </div>
-    </div>
-  );
-};
+const Collection = ({ match }) => (
+  <CollectionsContext.Consumer>
+    {
+      (collections) => {
+        const collection = collections[match.params.collectionId];
+        const { title, items } = collection;
+        return (
+          <div className="collection-page">
+            <h2 className="title">{title}</h2>
+            <div className="items">
+              {
+                items.map((item) => <CollectionItem key={item.id} item={item} />)
+              }
+            </div>
+          </div>
+        );
+      }
+    }
+  </CollectionsContext.Consumer>
+);
 
 Collection.propTypes = {
   collection: PropTypes.shape({
     title: PropTypes.string,
     items: PropTypes.array,
   }).isRequired,
+  match: PropTypes.objectOf(PropTypes.object).isRequired,
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state),
-});
-
-export default connect(mapStateToProps)(Collection);
+export default Collection;
