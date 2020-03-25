69e8c330995664c0c822f7ebabaea9fe3a9c1047
Kuei <vx3012564897@gmail.com>
2020-03-25 13:35:40 +0800

    build out backend for stripe payment

--- client/src/pages/collection/collection.styles.jsx
+++ client/src/pages/collection/collection.styles.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const CollectionPageContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 export const CollectionTitle = styled.h2`
