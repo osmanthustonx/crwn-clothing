c831e20649a8f27cfa6812557a8cae9ad568b3d0
Kuei <vx3012564897@gmail.com>
2020-03-27 03:00:41 +0800

    let compression and enforce.HTTPS performed in the production

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(cors());
-app.use(enforce.HTTPS({ trustProtoHeader: true})); 
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true})); 
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
